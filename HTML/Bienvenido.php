9cd7d61aabae8d3752186d0bc2c412cb0d592315
TheLuizZ <luizzjm9@gmail.com>
2022-09-27 11:43:11 -0600

    'holas'

--- HTML/Bienvenido.php
+++ HTML/Bienvenido.php
@@ -3,6 +3,87 @@ include("conexion.php");
 
 ?>
 <?php include 'barralateralinicial.php';?>
+
+<title>Inicio</title>
+<body data-ng-app="validationApp">
+<br>
+    <section  style=" background-color:rgb(255, 255, 255);
+    padding: 15px;
+    color:black;
+    font-size: 20px; " >
+      
+      <div class="content" >
+        <div class="content-header" >
+            <div class="row">
+                <h2>Acceso Directo</h2>
+               
+                <!-- /.col-lg-12 -->
+              </div>
+              <!-- /.row -->
+              <br>
+              <div class="row">
+                
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                      <div class="panel-heading">
+                        <div class="row">
+                          <div class="col-xs-3">
+                            <i class="fa fa-book fa-5x"></i>
+                          </div>
+                          <div class="col-xs-9 text-right">
+                            <div class="huge"><h1></h1></div>
+                            <div>Inventarios</div>
+                          </div>
+                        </div>
+                      </div>
+                      <a href="gestiones/Gestion_Inventario.php">
+                        <div class="panel-footer">
+                          <span class="pull-left">Ver Detalles</span>
+                          <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                          <div class="clearfix"></div>
+                        </div>
+                      </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary" >
+                      <div class="panel-heading" style="background:rgb(62, 226, 130);">
+                        <div class="row" >
+                          <div class="col-xs-3">
+                            <i class="fa fa-folder fa-5x"></i>
+                          </div>
+                          <div class="col-xs-9 text-right">
+                            <div class="huge"><h1></h1></div>
+                            <div>Facturacion</div>
+                          </div>
+                        </div>
+                      </div>
+                      <a href="demo.php">
+                        <div class="panel-footer">
+                          <span class="pull-left" style="color:rgb(62, 226, 130);">Ver Detalles</span>
+                          <span class="pull-right" style="color:rgb(62, 226, 130);"><i class="fa fa-arrow-circle-right"></i></span>
+                          <div class="clearfix"></div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  
+                  </div>
+                
+              </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        </div>
+    </section>
+</div>
+
 <script>
 $('.message a').click(function(){
   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
