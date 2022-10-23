dc492584d956b4bbf74d5645e80197b7e102773f
josegarcia0909 <93798434+jdgarciaunah09@users.noreply.github.com>
2022-10-22 23:57:58 -0600

    Update Gestion_Factura.php
    jajaj

--- HTML/gestiones/Gestion_Factura.php
+++ HTML/gestiones/Gestion_Factura.php
@@ -90,7 +90,7 @@ if (!isset($_SESSION['rol'])) {
 
                                 $desde = ($pagina-1) * $por_pagina;
                                 $total_paginas = ceil($total_registro / $por_pagina);
-                                    $sql = mysqli_query($conn,"select * FROM FACTURA LIMIT $desde,$por_pagina ");
+                                    $sql = mysqli_query($conn,"select * FROM FACTURA ORDER BY NFACTURA DESC LIMIT $desde,$por_pagina ");
                                     mysqli_close($conn);
 
 			                        $result = mysqli_num_rows($sql);
