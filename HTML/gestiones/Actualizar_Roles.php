9367faa4fc9a24d9f4eccdbf5bc7e71cd6e5df2a
hernandez25 <114378596+hernandez25@users.noreply.github.com>
2022-10-23 01:41:19 -0600

    Add files via upload

---
+++ HTML/gestiones/Actualizar_Roles.php
@@ -0,0 +1,171 @@
+<?php 
+	include ("../conexion.php");
+
+	if(!empty($_POST))
+	{
+		$alert='';
+		if(empty($_POST['Rol']) || empty($_POST['Descripcion']))
+		{ 
+			$alert='';
+			$alert='<p class="msg_error">Todos los campos son obligatorios.</p>';
+		}else{
+			$Id_Rol       = $_POST['Id_Rol'];
+			$Rol          = $_POST['Rol'];
+			$Estado       = $_POST['Estado'];
+			$Descripcion  = $_POST['Descripcion'];
+            if(!preg_match("/^[a-z A-Z \s  ñÑ+áéíóú]+$/" ,$Descripcion)){
+				echo "<script> alert('La descripción solo recibe letras');window.location= 'Actualizar_Roles.php' </script>";
+            }else{
+
+            $query = mysqli_query($conn,"UPDATE TBL_ROLES SET Estado='$Estado', Descripcion='$Descripcion' WHERE Id_Rol ='$Id_Rol'");
+
+				if($query){
+					echo "<script> alert('Rol Actualizado Exitosamente');window.location= 'GestionRoles.php' </script>";
+				}else{
+					$alert='<p class="msg_error">Error al actualizar la rol.</p>';
+				}
+			}
+        }
+	}
+
+	//Mostrar Datos
+	if(empty($_REQUEST['Id']))
+	{
+		header('Location: GestionRoles.php');
+	}
+	$Id = $_REQUEST['Id'];
+
+	$sql= mysqli_query($conn,"SELECT *	FROM TBL_ROLES WHERE Id_Rol = $Id");
+	$result_sql = mysqli_num_rows($sql);
+
+	if($result_sql == 0){
+		header('Location: GestionRoles.php');
+	}else{
+		while ($data = mysqli_fetch_array($sql)) {
+			# code...
+			$Id_Rol       = $data['Id_Rol'];
+			$Rol          = $data['Rol'];
+			$Estado       = $data['Estado'];
+			$Descripcion  = $data['Descripcion'];
+		}
+	}
+
+ ?>
+
+<?php
+//FUNCION DE PERMISOS
+
+//incluir las funciones de helpers
+include_once("../helpers/helpers.php");
+
+//iniciar las sesiones
+session_start();
+   // si no existe la variable rol, el usuario no esta logueado y redirige al Login
+if (!isset($_SESSION['rol'])) {
+   header("Location: ../login.php"); 
+   die();
+}else{
+   //actualiza los permisos
+   updatePermisos($_SESSION['rol']);
+   
+   //si no tiene permiso de visualización redirige al index
+   if ($_SESSION['permisos'][M_GESTION_ROLES]['u']==0 or !isset($_SESSION['permisos'][M_GESTION_ROLES]['u'])) {
+       header("Location: ../index.php");
+       die();
+   }
+}
+?>
+
+<?php include 'barralateralinicial.php';?>
+  </div>
+  <div class="container mt-12">
+                  <div class="col-md-12">
+	<title>ACTUALIZAR ROL</title>
+  <h6><a  class="btn btn-primary"  href="GestionRoles.php ">Volver Atrás</a></h6>
+</head>
+<body>
+	<section id="container">
+		<div class="form_register">
+			
+			<form action="" method="post">
+			<h1>Actualizar Rol</h1>
+			<hr>
+                <input type="hidden" name="Id_Rol" value="<?php echo $Id_Rol  ?>">
+				<label for="Rol">Nombre Rol</label>
+				<input type="text" name="Rol" maxlength="50" id="Rol" placeholder="Nombre" readonly= "true" value ="<?php echo $Rol?>">
+				<label for="Estado">Estado</label>
+                  <select name="Estado" required>
+                  <option value ="">Seleccione Una Opción</option>
+                  <option value="ACTIVO">ACTIVO</option>
+                  <option value="INACTIVO">INACTIVO</option>
+                  </select>
+				<label for="Descripcion">Descripción</label>
+				<input type="text" name="Descripcion" maxlength="50" id="Descripcion" placeholder="Descripción" value ="<?php echo $Descripcion ?>" size = 50 required>
+				<br>
+				<input type="submit" value="Actualizar Rol" class="btn_save">
+			</form>
+		</div>
+	</section>
+</body>
+<style type="text/css">
+.btn-atras{
+	background: #1faac8;
+	color: #FFF;
+	padding: 0 20px;
+	border: 0;
+	cursor: pointer;
+	margin-left: 20px;
+}
+.form_register{
+	width: 450px;
+	margin: auto;
+}
+.form_register h1{
+	color: #3c93b0;
+}
+hr{
+	border: 0;
+	background: #CCC;
+	height: 1px;
+	margin: 10px 0;
+	display: block;
+}
+form{
+	background: #FFF;
+	margin: auto;
+	padding: 20px 50px;
+	border: 1px solid #d1d1d1;
+}
+label{
+	display: block;
+	font-size: 12pt;
+	font-family: 'GothamBook';
+	margin: 15px auto 5px auto;
+}
+.btn_save{
+	font-size: 12pt;
+	background: #12a4c6;
+	padding: 10px;
+	color: #FFF;
+	letter-spacing: 1px;
+	border: 0;
+	cursor: pointer;
+	margin: 15px auto;
+}
+.alert{
+	width: 100%;
+	background: #66e07d66;
+	border-radius: 6px;
+	margin: 20px auto;
+}
+.msg_error{
+	color: #e65656;
+}
+.msg_save{
+	color: #126e00;
+}
+.alert p{
+	padding: 10px;
+}
+</style>
+<?php include 'barralateralfinal.php';?>
