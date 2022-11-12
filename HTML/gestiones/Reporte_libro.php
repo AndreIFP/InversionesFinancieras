ce114a0003d9287d7b394223af2bdb1533a4c15d
hernandez25 <114378596+hernandez25@users.noreply.github.com>
2022-11-12 03:41:34 -0600

    Add files via upload

---
+++ HTML/gestiones/Reporte_libro.php
@@ -0,0 +1,183 @@
+<?php 
+
+session_start();
+$_SESSION['id'];
+$_SESSION['cliente'];
+$_SESSION['fechai'];
+$_SESSION['fechaf'];
+$_SESSION['temporada']="10";
+
+
+
+
+?>
+
+<?php
+
+require('fpdf.php');
+require ('../conexion.php');
+
+class PDF extends FPDF
+{
+// Cabecera de página
+function Header()
+{
+  require ('../conexion.php');
+    // Logo
+    $this->Image('logO.PNG',92,32,35);
+    // Arial bold 15
+    $this->SetFont('Arial','B',18);
+    // Movernos a la derecha
+    $this->Cell(86);
+    // Título
+    $this->Cell(22,10,'Inversiones Financieras IS',2,0,'C');
+    $this->Ln(5);
+    
+    // Llamado del parametro dirección
+    $sqldireccion = "SELECT * FROM TBL_PARAMETROS WHERE Id_Parametro = '4'";
+    $resultadodir = mysqli_query($conn,$sqldireccion);
+    while ($fila = $resultadodir->fetch_assoc()) {
+        $Direccion = $fila["Valor"];
+    }
+
+    $this->SetFont('Arial','',8);
+    $this->Cell(62);
+    $this->Cell(8,10, utf8_decode($Direccion),0,7, 45);
+    $this->Ln(0);
+    
+    // Llamado del parametro telefono
+    $sqlTelefono = "SELECT * FROM TBL_PARAMETROS WHERE Id_Parametro = '3'";
+    $resultadotel = mysqli_query($conn,$sqlTelefono);
+    while ($fila = $resultadotel->fetch_assoc()) {
+        $Telefono = $fila["Valor"];
+    }
+
+    $this->SetFont('Arial','',8);
+    $this->Cell(79);
+    $this->Cell(8,0, utf8_decode('Teléfono: ' .$Telefono ),0,7);
+    $this->Ln(4);
+    
+    // Llamado del parametro correo
+    $sqlCorreo = "SELECT * FROM TBL_PARAMETROS WHERE Id_Parametro = '2'";
+    $resultadocorreo = mysqli_query($conn,$sqlCorreo);
+    while ($fila = $resultadocorreo->fetch_assoc()) {
+        $Correo = $fila["Valor"];
+    }
+
+    $this->SetFont('Arial','',8);
+    $this->Cell(76);
+    $this->Cell(8,0, utf8_decode('Email: '.$Correo),0,7);
+    $this->Ln(3);
+
+    $this->SetFont('Arial','',14);
+    $this->Cell(75);
+    $this->Cell(10,60, utf8_decode('Reporte Libro Diario' ),0,7);
+    $this->Ln(-53);
+
+    
+    $this->SetFont('Arial','',14);
+    $this->Cell(68);
+    $this->Cell(10,60, utf8_decode('La empresa' ),0,7);
+    $this->Ln(-30);
+
+    $this->SetFont('Arial','',14);
+    $this->Cell(96);
+    $this->Cell(8,0, utf8_decode($empresa=$_SESSION['empresa'] ),0,7);
+    $this->Ln(2);
+       
+    $this->Ln(5); 
+    
+}
+
+// Pie de página
+function Footer()
+{
+    // Mostrar el usuario que impime el reporte
+    $this->SetY(-15);
+    $this->SetFont('Arial','I',8);
+    // Movernos a la derecha
+    $this->Cell(0,15,utf8_decode('Reporte creado por: '.$user=$_SESSION['user']),2,0,'T');
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,15,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    $Object = new DateTime();  
+    $Object->setTimezone(new DateTimeZone('America/Guatemala'));
+    $DateAndTime = $Object->format("d-m-Y h:i:s a");
+    $this->Cell(0,15,$DateAndTime,0,1,'R');
+}
+}
+
+
+$id_usuario=$_SESSION['id'];
+$cliente=$_SESSION['cliente'];
+$temporada=$_SESSION['temporada'];
+$fechai=$_SESSION['fechai'];
+$fechaf=$_SESSION['fechaf'];
+        $fecha = date('Y-m-d h:i:s');
+
+
+//  if ($guardar=="si") {
+
+  
+// Creación del objeto de la clase heredada
+$sql = "SELECT * from libro where id_cliente='$cliente' and fecha >='$fechai' and fecha <='$fechaf'; ";
+$resultado = mysqli_query($conn,$sql);
+
+
+  
+    
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->SetMargins(10,10,10);
+$pdf->AddPage();
+
+
+$pdf->SetFont('Times','B',8);
+$pdf->setX(10);
+
+
+
+$pdf->SetFillColor(108, 250, 254 );
+$pdf->Cell(40,5, utf8_decode('Fecha'),1,0,'C',1);
+$pdf->Cell(50,5, utf8_decode('Cuenta'),1,0,'C',1);
+$pdf->Cell(50,5, utf8_decode('Descripción'),1,0,'C',1);
+$pdf->Cell(25,5, utf8_decode('Debe'),1,0,'C',1);
+$pdf->Cell(25,5, utf8_decode('Haber'),1,1,'C',1);
+
+
+
+while ($fila = $resultado->fetch_assoc()) {
+    $monto_haber=0;
+    $monto_debe=0;
+    $monto=$fila['monto'];
+    $id_libro=$fila['id_libro'];
+    $debe_haber=$fila['debe_haber'];
+     
+  if($debe_haber=="debe"){
+    $monto_debe=$fila['monto'];
+
+      }
+  if($debe_haber=="haber"){
+    $monto_haber=$fila['monto'];
+
+      }
+    $pdf->setX(10);
+    $pdf->Cell(40, 5, utf8_decode($fila['fecha']), 1, 0, "C",0);
+    $pdf->Cell(50, 5, utf8_decode($fila['cuenta']), 1, 0, "B",0);
+    $pdf->Cell(50, 5, utf8_decode($fila['descripcion']), 1, 0, "B",0);
+    $pdf->Cell(25, 5, utf8_decode( $monto_debe), 1, 0, "C",0);
+    $pdf->Cell(25, 5, utf8_decode($monto_haber), 1, 1, "C",0);
+   
+
+
+}
+
+
+
+
+
+$pdf->Output();
+?>
