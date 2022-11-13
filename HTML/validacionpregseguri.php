56c4e1a86547658bbd595c9aebf2d09f48c30bc5
josegarcia0909 <93798434+jdgarciaunah09@users.noreply.github.com>
2022-11-13 01:08:51 -0600

    Update validacionpregseguri.php
    SEFF

--- HTML/validacionpregseguri.php
+++ HTML/validacionpregseguri.php
@@ -87,7 +87,7 @@ if(isset($_REQUEST["btnregistrarx"])){
                     $sql = "UPDATE TBL_USUARIO SET Estado_Usuario='BLOQUEADO' WHERE Id_Usuario='$iduser'";
                     $con = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
                     $exito = mysqli_query($con,$sql);
-                    echo "<script>alert('DEMASIADOS INTENTOS EL USUARIO ".$nombre." SE HA BLOQUEADO');window.location= 'login.php'</script>";
+                    echo "<script>alert('USUARIO ".$nombre." SE HA BLOQUEADO CONSULTE CON SU ADMINISTRADOR');window.location= 'login.php'</script>";
     }else{
         $_SESSION["intentos"]++; // aumentamos en 1 los intentos
         echo "<script>alert('Ingreso invalido, PORFAVOR REVISE SUS CREDENCIALES DE INICIO DE SESION);window.location= 'login.php'</script>";
