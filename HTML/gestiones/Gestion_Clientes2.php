eec817f6a73270aad48e03f19670d3abe7c2c31b
SarahOrtiz28 <114377972+SarahOrtiz28@users.noreply.github.com>
2022-11-27 15:02:42 -0600

    Update Gestion_Clientes2.php

--- HTML/gestiones/Gestion_Clientes2.php
+++ HTML/gestiones/Gestion_Clientes2.php
@@ -253,9 +253,9 @@ $query = mysqli_query($conn,"SELECT * FROM TBL_CLIENTES WHERE Id_Cliente = '$pol
   <input type="text" class="form-control" Readonly  id="recipient-name" value=" <?php echo $Tipo_Cliente?> ">
   </div>
 	
-  <div class= "form group">
+  div class= "form group">
   <label for="Ciudad">Ciudad</label>
-  <input type="text" name="Ciudad" maxlength="25" id="Ciudad" placeholder="Ciudad"  value ="<?php echo $Ciudad ?>"size="40" onkeyup="javascript:this.value=this.value.toUpperCase();"   required>
+  <input type="text" class="form-control" Readonly  id="recipient-name"  value ="<?php echo $Ciudad?> ">
   </div>
 	
   <?php
