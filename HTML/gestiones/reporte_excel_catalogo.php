a85c4be170e2728b12aaaa2acf85208acb4c2226
josegarcia0909 <93798434+jdgarciaunah09@users.noreply.github.com>
2022-11-27 21:18:10 -0600

    Add files via upload

--- HTML/gestiones/reporte_excel_catalogo.php
+++ HTML/gestiones/reporte_excel_catalogo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> <!--Importante--->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reporte Excel</title>
+    <title>Descargar</title>
     
 
 </head>
@@ -18,15 +18,11 @@ header("Content-Type: application/vnd.ms-excel charset=iso-8859-1");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Content-Disposition: attachment; filename= Catálogo De Cuentas.xls");
 
-session_start();
 
-date_default_timezone_set("America/Guatemala");
-$fecha = date("d-m-Y h:i:s a");
 
-$busqueda=$_GET['busqueda_filtro'];
-// Creación del objeto de la clase heredada
-$sql = mysqli_query($conn, "SELECT * FROM TBL_CATALAGO_CUENTAS WHERE ( CODIGO_CUENTA LIKE '%$busqueda%' OR
-                                                                        CUENTA  LIKE '%$busqueda%'  OR Estado_Cuenta  LIKE '%$busqueda%')");
+
+$sql = "SELECT * FROM TBL_CATALAGO_CUENTAS WHERE Estado_Cuenta = 'ACTIVO'";
+$DataPaises = mysqli_query($conn, $sql);
 
 // Llamado del parametro dirección
 $sqldireccion = "SELECT * FROM TBL_PARAMETROS WHERE Id_Parametro = '4'";
@@ -60,31 +56,33 @@ while ($fila = $resultadocorreo->fetch_assoc()) {
 <label><center>Dirección: <?php echo $Direccion ?></center></label>
 <label><center>Teléfono: <?php echo $Telefono ?></center></label>
 <label><center>Email: <?php echo $Correo ?></strong></center></label>
-<label><center>Catálogo De Cuentas</center></label>
+<label><center>Reporte Catálogo Cuentas</center></label>
 
 <br>
 <table style="text-align: center;" border='1' cellpadding=1 cellspacing=1>
 <thead>
+
+
+
     <tr style="background: #B0E0E6;">
-    <th>Código Cuenta</th>
+    <th>Código</th>
     <th>Cuenta</th>
+    <th>Clasificación</th>
     <th>Estado Cuenta</th>
     </tr>
 </thead>
 <?php
-    while ($row = mysqli_fetch_array($sql)) { ?>
+    while ($row = mysqli_fetch_array($DataPaises)) { ?>
     <tbody>
         <tr>
         <td><?php echo $row['CODIGO_CUENTA'] ?></td>
         <td><?php echo $row['CUENTA'] ?></td>
+        <td><?php echo $row['CLASIFICACION'] ?></td>
         <td><?php echo $row['Estado_Cuenta'] ?></td>
         </tr>
     </tbody>
     
 <?php } ?>
 </table>
-<br>
-<label>Reporte creado por: <?php echo $user=$_SESSION['user'] ?></label>
-<label> <?php echo $fecha ?></strong></label>
 </body>
 </html>
