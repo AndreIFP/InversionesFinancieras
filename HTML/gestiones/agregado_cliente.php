f020208853d93760db9d327b75566fcbd8f9b195
josegarcia0909 <93798434+jdgarciaunah09@users.noreply.github.com>
2022-12-06 23:48:53 -0600

    Update agregado_cliente.php
    drgdrg

--- HTML/gestiones/agregado_cliente.php
+++ HTML/gestiones/agregado_cliente.php
@@ -12,7 +12,7 @@
 			$query_insert = mysqli_query($conn,"INSERT INTO TBL_CLIENTES(Nombre_Empresa,Nombre_Cliente,RTN_Cliente,Direccion,Telefono,Tipo_Cliente,Ciudad)
 																	VALUES('$nombren','$nombre','$RTN','$Direccion','$Telefono','$Tipo_Cliente','$Ciudad')");
 			    if($query_insert){
-					echo "<script> alert('Cliente Registrado Exitosamente');window.location= 'Gestion_Clientes.php' </script>";
+					echo "<script> alert('Cliente Registrado Exitósamente');window.location= 'Gestion_Clientes.php' </script>";
 				}else{
 					$alert='<p class="msg_error">Error al registrar el cliente.</p>';
 				}
