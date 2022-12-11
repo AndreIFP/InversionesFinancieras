112a4295cdcc3a263af8cb5d16f80c63b56d5c72
hernandez25 <114378596+hernandez25@users.noreply.github.com>
2022-12-10 23:58:38 -0600

    Update Gestion_Usuarios2.php

--- HTML/gestiones/Gestion_Usuarios2.php
+++ HTML/gestiones/Gestion_Usuarios2.php
@@ -161,7 +161,7 @@ $numero = 99999.99;
                     <tbody>
                         <?php
                         //Paginador
-                        $sql_registe = mysqli_query($conn, "SELECT COUNT(*) as total_registro FROM tbl_usuario WHERE Id_Usuario = Id_Usuario AND Estado_Usuario = 'Activo' ");
+                        $sql_registe = mysqli_query($conn, "SELECT COUNT(*) as total_registro FROM tbl_usuario WHERE Id_Usuario = Id_Usuario AND Estado_Usuario = 'Activo' or  Estado_Usuario = 'NUEVO' ");
                         $result_register = mysqli_fetch_array($sql_registe);
                         $total_registro = $result_register['total_registro'];
 
@@ -179,7 +179,7 @@ $numero = 99999.99;
 
                         $desde = ($pagina - 1) * $por_pagina;
                         $total_paginas = ceil($total_registro / $por_pagina);
-                        $sql = mysqli_query($conn, "select u.Id_Usuario, u.Usuario, u.Nombre_Usuario, u.Estado_Usuario, u.Correo_Electronico, u.Fecha_Ultimo_Conexion, r.Rol from tbl_usuario u inner join tbl_roles r ON u.Rol = r.Id_Rol WHERE Estado_Usuario = 'Activo' ORDER BY u.Id_Usuario DESC LIMIT $desde,$por_pagina ");
+                        $sql = mysqli_query($conn, "select u.Id_Usuario, u.Usuario, u.Nombre_Usuario, u.Estado_Usuario, u.Correo_Electronico, u.Fecha_Ultimo_Conexion, r.Rol from tbl_usuario u inner join tbl_roles r ON u.Rol = r.Id_Rol WHERE Estado_Usuario = 'Activo' or  Estado_Usuario = 'NUEVO' ORDER BY u.Id_Usuario DESC LIMIT $desde,$por_pagina ");
                         mysqli_close($conn);
 
 
